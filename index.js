92e27ebee85f06f1a9bdea88e8dce4bba68e1586
rethabile <rethabilemakaotsi17@gmail.com>
2023-11-20 10:56:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+ function greet (name){
+    console.log(  "hello" + name);
+ } 
+ greet ('rethabile');
+
+ function multiply(a,b){
+    console.log(a*b);
+ }
+ multiply(7,3)
+
+
+ function tableOfFive(number){
+    console.log(number)
+    return number*5;
+ }
+let results = tableOfFive(7)
+console.log(results);
